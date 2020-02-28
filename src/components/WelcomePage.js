8866da6e69492e4cb80c27e913f97da52e734778
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-02-28 10:58:42 -0800

    done

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function WelcomePage() {
+
+  const Picture = styled.div`
+    margin: 0% 35%;
+  `
   return (
+
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
+        <Picture>
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
+        </Picture>
       </header>
     </section>
   );
